4b509efbb397809ca9fc056aca40ec2c104b329f
yarinr <y3arin@gmail.com>
2017-11-05 10:10:44 +0300

    define app's api

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+let heroes = [];
+
+app.get('/heroes', (req, res) => {});
+
+app.get('/heroes/:id', (req, res) => {});
+
+app.put('/heroes/:id', (req, res) => {});
+
+app.post('/heroes', (req, res) => {});
+
+app.delete('/heroes/:id', (req, res) => {});
+
+app.delete('/heroes?name=term', (req, res) => {});
 
 app.listen(3000, () => console.log('ToH app listening on port 3000!'))
